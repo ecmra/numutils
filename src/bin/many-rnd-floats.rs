0363cb3ea4897f7d9f9c63e96435630c1d5d56c7
ecmra <emanuele.raineri@cnag.eu>
2026-07-13 13:37:07 +0200

    - generate 1M floats

--- src/bin/many-rnd-floats.rs
+++ src/bin/many-rnd-floats.rs
@@ -2,7 +2,7 @@ use numutils::ch7;
 
 fn main (){
     let mut seed = 3u32;
-    for _i in 0..100_000{
+    for _i in 0..1_000_000{
 	let r = seed as f32/u64::pow(2,32) as f32;
 	println!("{}", r);
 	//println!("{}", seed as f32/u32::pow(2,32) as f32);
